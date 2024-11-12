b3a6c2eb269cf06b2f66553ef3426d5ca0890b06
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2024-11-12 09:34:40 +0100

    itemek lekérése, dinamikus generálás

--- modules/core.js
+++ modules/core.js
@@ -45,6 +45,25 @@ router.get('/kolcsonzo', (req, res) => {
     res.redirect('/');
 });
 
+//Könyv kölcsönzés lekérés
+router.get('/api/items', (req, res) => {
+    const query = `
+      SELECT item_id, title, type
+      FROM items
+      WHERE available = true;
+    `;
+    
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error fetching items:', err);
+        res.status(500).send('Database error');
+        return;
+      }
+      
+      res.json(results);
+    });
+  });
+
 router.get('/statistics', (req, res) => {
     if (req.session.isLoggedIn) {
         db.query(`SELECT * FROM stepdatas WHERE userID=? ORDER BY date ASC`, [req.session.userID], (err, results) => {
